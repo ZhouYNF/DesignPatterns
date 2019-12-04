faa28689b401b00884ba8da615e014ff5e2a894a
zhouY <1010356543@qq.com>
2019-12-04 13:36:32 +0800

    外观模式

---
+++ src/facade/Client.java
@@ -0,0 +1,21 @@
+package facade;
+
+/**
+ * 外观模式：为子系统中的一组接口提供一个一致的界面，此模式定义了一个高层接口，这个接口使得这一子系统更加容易使用
+ *
+ * 就是定义统一的接口，
+ */
+public class Client {
+
+	public static void main(String[] args) {
+
+
+		HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade();
+		homeTheaterFacade.ready();
+		homeTheaterFacade.play();
+		
+		
+		homeTheaterFacade.end();
+	}
+
+}
